919695372bac0650cde4d6049c04fbe0dfb2c908
Michael C. Browne <mcbrowne@slac.stanford.edu>
2012-05-16 23:21:09 +0000

    Minor typo in print statement.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/modules/ipimb/current@9637 ececa60f-9f63-0410-97c7-ea05888eb260

--- ipimbLib/src/IpimBoard.cc
+++ ipimbLib/src/IpimBoard.cc
@@ -513,7 +513,7 @@ void IpimBoard::do_read()
             if (in_sync) {
                 status = evrTimeGetFifo(&ts[dbuf], trigevent, &idx, incr);
                 if (status) {
-                    printf("IPIMB%d has an ancient timestamp, resynching!", _physID);
+                    printf("IPIMB%d has an ancient timestamp, resynching!\n", _physID);
                     fflush(stdout);
                     in_sync = 0;
                     do_print = 0;
